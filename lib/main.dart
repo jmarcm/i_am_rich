983338afa9f8ca466251399ce4255e1437572966
Jean-Marc <mjean_marc@hotmail.com>
2020-06-27 09:19:02 +0200

    Display "Hello World" centered

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Text("Hello World"),
+      home: Center(
+        child: Text("Hello World"),
+      ),
     ),
   );
 }
