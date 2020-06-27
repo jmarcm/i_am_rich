2c068f87cd069fc43afd9cc83145aad17ebc2b82
Jean-Marc <mjean_marc@hotmail.com>
2020-06-27 11:11:56 +0200

    Display Network Image

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("Hello World"),
+          title: Center(
+            child: Text("I am Rich"),
+          ),
           backgroundColor: Colors.blueGrey[900],
         ),
-        backgroundColor: Colors.blueGrey,
+        body: Image(
+          image: NetworkImage(
+              'https://lh6.googleusercontent.com/proxy/2xoq9BG7AQsRC9_hYNtqyCt4dBIWKMV1e95YFB7kIdCuj9zzGJce8CKA5RfQ_-BQ7FfERIhtWFBFgfCJ26d1ABN2i_5uYVLMbJH1OQ'),
+        ),
       ),
     ),
   );
