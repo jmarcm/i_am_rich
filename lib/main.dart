bbfbc96b0ca924ea897ad91ed473215c71349222
Jean-Marc <mjean_marc@hotmail.com>
2020-06-27 11:29:46 +0200

    Display Network Image Centered

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,11 @@ void main() {
           ),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Image(
-          image: NetworkImage(
-              'https://lh6.googleusercontent.com/proxy/2xoq9BG7AQsRC9_hYNtqyCt4dBIWKMV1e95YFB7kIdCuj9zzGJce8CKA5RfQ_-BQ7FfERIhtWFBFgfCJ26d1ABN2i_5uYVLMbJH1OQ'),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://lh6.googleusercontent.com/proxy/2xoq9BG7AQsRC9_hYNtqyCt4dBIWKMV1e95YFB7kIdCuj9zzGJce8CKA5RfQ_-BQ7FfERIhtWFBFgfCJ26d1ABN2i_5uYVLMbJH1OQ'),
+          ),
         ),
       ),
     ),
